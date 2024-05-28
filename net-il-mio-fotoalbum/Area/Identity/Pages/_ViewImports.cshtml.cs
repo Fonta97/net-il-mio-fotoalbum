0b72eb628ec71f8433ede1e954bb6482b3a1c97d
Fonta97 <fontana.alessandro97@gmail.com>
2024-05-28 17:40:31 +0200

    Aggiungere i file di progetto.

---
+++ net-il-mio-fotoalbum/Area/Identity/Pages/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace net_il_mio_fotoalbum.Area.Identity.Pages
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
